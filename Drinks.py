fdc733240b0e551388c477f87fdae72cbca88e12
mellamanmar <marimelissamc@gmail.com>
2023-05-09 17:30:06 -0500

    Agregando función get y set a la clase padre Drinks

--- Drinks.py
+++ Drinks.py
@@ -1,35 +1,39 @@
 class Drinks:
 
     def __init__(self, name, tipe, quantity):
-        self.name = name
+        self._name = name
         self.tipe = tipe
         self.quantity = quantity
     
     def atributos (self):
-        print (f"Tenemos {self.quantity} de {self.name} que es una bebida de tipo {self.tipe}")
+        print (f"Tenemos {self.quantity} de {self._name} que es una bebida de tipo {self.tipe}")
     
     def drinkUp (self):
         if self.tipe == "alcohólica" or self.tipe == "alcoholica":
             age= input ("¿Eres mayor de edad? ").upper()
             if age == "SI":
-                print ("Te tomas la/el",self.name)
+                print ("Te tomas la/el",self._name)
             elif age == "NO":
                 print ("Debes ser mayor de edad")
             else:
                 print ("Ingrese una respuesta válida")
         elif self.tipe == "jugo" or self.tipe == "refresco" or self.tipe == "agua":
-            print ("Te tomas la/el",self.name)
+            print ("Te tomas la/el",self._name)
         else:
             print ("El tipo de bebida no es válido")
 
-    def changeDrink (self, name):
-        self.name = name
-        print ("Ahora tu bebida es ",self.name)
+    def set_name (self, name):
+        self._name = name
+    
+    def get_name (self):
+        return self._name
 
 tequila=Drinks ("Tequila", "alcoholica", "1 copa")
 tequila.atributos ()
 tequila.drinkUp ()
-tequila.changeDrink ("vodka")
+tequila.set_name ("vodka")
+print ("Se cambia la bebida por",tequila.get_name())
+tequila.atributos ()
 
 class StrawberryJuice (Drinks):
     def __init__(self, name, tipe, quantity, sugary):
@@ -43,8 +47,15 @@ class StrawberryJuice (Drinks):
     def changeDrink(self, name):
         return super().changeDrink(name)
     
+    def sell (self, price):
+        self.price= price
+        if self.price > 0:
+            print (f"El {self.name} se venderá en {self.price}")
+        else:
+            print ("Ingrese un precio válido")
 
 strawberry1= StrawberryJuice ("Jugo de fresa", "jugo", "1 vaso", 20)
 strawberry1.atributos ()
 strawberry1.changeDrink ("parchita")
 strawberry1.atributos ()
+strawberry1.sell (int(input ("¿Cuál es el precio de la bebida? ")))
