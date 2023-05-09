dc41af446d2533546908b583c18feba876fef5ea
mellamanmar <marimelissamc@gmail.com>
2023-05-09 18:30:33 -0500

    Modificando el código de Texts, clase padre y clase hija

--- Texts.py
+++ Texts.py
@@ -1,31 +1,59 @@
 class Texts:
-    def __init__(self, large, idiom):
-        self.large= large
+    def __init__(self, paragraphs, idiom):
+        self.paragraphs= paragraphs
         self.idiom= idiom
-    
+
+    def attributes (self):
+        print (f"El texto está escrito en {self.idiom} y tiene {self.paragraphs} párrafos")
+
     def copy (self):
         if self.idiom == "español" or self.idiom == "Español":
             print ("El texto será copiado")
         else:
             print ("Se debe traducir el texto al español")
 
-    def change (self):
-        if self.large < 3:
+    def large (self):
+        if self.paragraphs < 3:
             print ("El texto no es válido, debe ser más largo")
-        if self.large > 20:
+        elif self.paragraphs > 6:
             print ("Que no exceda de 5 párrafos")
-        
-text1= Texts (input ("¿De cuántos párrafos es tu cuento? "), input("¿En qué idioma esta escrito? "))
-text1.copy()
-
-class StoryTale(Texts):
-    def __init__(self, large, idiom, type):
-        super().__init__(large, idiom)
-        self.type= type
     
-    def whatKind (self):
-        print (f"Te vamos a leer una historia de {self.type} en {self.idiom}")
-        return self.type
+        
+# text1= Texts (input ("¿De cuántos párrafos es tu texto? "), input("¿En qué idioma esta escrito? "))
+# text1.attributes ()
+# text1.copy()
+# text1.large ()
 
-story1= StoryTale (3, "español", "terror")
-story1.whatKind()
+class BBC (Texts):
+    def __init__(self, paragraphs, idiom, type, author):
+        super().__init__(paragraphs, idiom)
+        self.type= type
+        self.author= author
+
+    def who_write (self):
+        if self.author != False:
+            print (self.author)
+        else:
+            print ("Ingrese un autor del texto")
+
+    def large (self):
+        if self.paragraphs < 10:
+            print ("El texto no es válido, debe ser más largo")
+        elif self.paragraphs >= 30:
+            print ("Que no exceda de 30 párrafos")
+        else:
+            print ("Listo para publicarse")
+
+    def write (self):
+        if self.large == "Listo para publicarse":
+            return self.author
+    
+    def readers (self):
+        read= int (input ("¿Cuántas personas han leído el artículo hasta hoy? "))
+        print ("El artículo ha sido leído por",read,"personas")            
+    
+bbc1= BBC (40, "español", "finanzas", "Gilberto Rojas")
+bbc1.who_write ()
+bbc1.readers ()
+bbc1.large ()
+bbc1.write ()
